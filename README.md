6098fe614b4bf5a7cd5d1add14c452bafcb4bff2
luffy2106 <tkdang@assystem.com>
2024-04-05 15:55:57 +0200

    finish workflow_automation

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# airflow
+# apache_airflow
+
+### Installation
+Install Apache Airflow by this tutorial
+```
+https://airflow.apache.org/docs/apache-airflow/stable/start.html
+```
+
+You can set up your own account by run this command in the terminal 
+```
+airflow db migrate
+
+airflow users create \
+    --username admin \
+    --firstname Peter \
+    --lastname Parker \
+    --role Admin \
+    --password your_password_here \
+    --email spiderman@superhero.org
+
+airflow webserver --port 8080
+
+airflow scheduler
+```
+
+
+### Dynamic workflows
+
+In Airflow, dynamic workflows refer to the ability to generate tasks dynamically during runtime based on certain conditions or parameters. This allows for more flexible and customizable workflow orchestration.
+
+#####  Scenario
+You can schedule tasks by based on internal trigger or external trigger. For example :
+- Internal trigger : You build a deep learning model but the data keep changing overtime, in this case, you can trigger the model retrained by airflow every month, to update the performance of the model.
+- External trigger : You can define external triggers that determine whether a task should be executed based on the outcome of a previous task or an external condition. Such as when you have new annotation data coming, you can trigger to train model whenever we have more than X data available.
+
+##### Implementation
+
+For the illustration, we can use some random dataset online, should choose the data which is not complicated.
+
+Context :
+Define tasks for each step in the workflow:
+- Task 1: Download data files from an FTP server using a shell script.
+- Task 2: Preprocess the data using Python scripts.
+- Task 3: Train a machine learning model using a Jupyter notebook or Python script.
+- Task 4: Generate reports based on the model predictions.
+
+
+The pipeline should run one per week, to make sure that we have updated model with updated data all the time. Each task will be excuted if and only if the preceeding task finish.
+
---
+++ wokrflow_automation.py
@@ -0,0 +1,91 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import time
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2022, 1, 1),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'my_data_pipeline',
+    default_args=default_args,
+    description='A simple data pipeline',
+    schedule_interval=timedelta(weeks=1),  # Run the DAG weekly
+)
+
+def download_data():
+    start_time = time.time()
+    
+    # Run the function for 5 seconds
+    while time.time() - start_time < 5:
+        # Your function logic goes here
+        pass
+    # Code for preprocessing data goes here
+    print("download data step")
+
+
+
+def preprocess_data():
+    start_time = time.time()
+    
+    # Run the function for 5 seconds
+    while time.time() - start_time < 5:
+        # Your function logic goes here
+        pass
+    # Code for preprocessing data goes here
+    print("Preprocess data step")
+
+def train_model():
+    start_time = time.time()
+    
+    # Run the function for 5 seconds
+    while time.time() - start_time < 5:
+        # Your function logic goes here
+        pass
+    # Code for training the model goes here
+    print("Train model step")
+
+def generate_reports():
+    start_time = time.time()
+    
+    # Run the function for 5 seconds
+    while time.time() - start_time < 5:
+        # Your function logic goes here
+        pass
+    
+    # Code for generating reports goes here
+    print("Generate reports step")
+
+
+download_data_task = PythonOperator(
+    task_id='download_data',
+    python_callable=download_data,
+    dag=dag,
+)
+
+preprocess_data_task = PythonOperator(
+    task_id='preprocess_data',
+    python_callable=preprocess_data,
+    dag=dag,
+)
+
+train_model_task = PythonOperator(
+    task_id='train_model',
+    python_callable=train_model,
+    dag=dag,
+)
+
+generate_reports_task = PythonOperator(
+    task_id='generate_reports',
+    python_callable=generate_reports,
+    dag=dag,
+)
+
+# Setting up task dependencies
+download_data_task >> preprocess_data_task >> train_model_task >> generate_reports_task
